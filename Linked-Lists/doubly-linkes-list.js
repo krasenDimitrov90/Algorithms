02281d5eb8337d9b89b858ad5c25e2cb9955c9a2
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-07-03 22:42:08 +0300

    [DLL] pop method

--- Linked-Lists/doubly-linkes-list.js
+++ Linked-Lists/doubly-linkes-list.js
@@ -24,6 +24,25 @@ class DoublyLinkedList {
             newNode.prev = this.tail;
             this.tail = newNode;
         }
+        return newNode;
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+
+        let popedNode = this.tail;
+
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = popedNode.prev;
+            this.tail.next = null;
+            popedNode.prev = null;
+        }
+        this.length--;
+
+        return popedNode;
     }
 
     printList() {
@@ -40,6 +59,7 @@ class DoublyLinkedList {
 let doublyLinkedList = new DoublyLinkedList();
 doublyLinkedList.push(1);
 doublyLinkedList.push(2);
-doublyLinkedList.push(3);
+// doublyLinkedList.push(3);
+console.log(doublyLinkedList.pop());
 
 doublyLinkedList.printList();
