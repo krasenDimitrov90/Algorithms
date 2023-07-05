fc2babbc5a80ab3a016f5bc24b0c1a0296761b8c
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-07-05 23:20:57 +0300

    [Binary Tree] find method

--- Binary-Trees/binary-tree.js
+++ Binary-Trees/binary-tree.js
@@ -11,7 +11,7 @@ class BinarySearchTree {
         this.root = null;
     }
 
-    insertOne(val) {
+    insert(val) {
         let newNode = new Node(val);
         if (!this.root) {
             this.root = newNode;
@@ -19,14 +19,14 @@ class BinarySearchTree {
         }
 
         let currentNode = this.root;
-        let newNodeHasAdded = false;
 
-        while (!newNodeHasAdded) {
+        while (true) {
             if (currentNode === val) return undefined;
             let leftNode = currentNode.left;
             let rightNode = currentNode.right;
 
             if (currentNode.val > val) {
+                // go to left
                 if (!leftNode) {
                     currentNode.left = newNode;
                     return newNode;
@@ -34,6 +34,7 @@ class BinarySearchTree {
                     currentNode = leftNode;
                 }
             } else if (currentNode.val < val) {
+                // go to right
                 if (!rightNode) {
                     currentNode.right = newNode;
                     return newNode;
@@ -43,17 +44,57 @@ class BinarySearchTree {
             }
         }
     }
+
+    find(val) {
+        let currentNode = this.root;
+        if (currentNode.val === val) return currentNode;
+
+        while (true) {
+            if (currentNode.val > val) {
+                // go to left
+                if (!currentNode.left) return undefined;
+                if (currentNode.left.val === val) return currentNode.left;
+                currentNode = currentNode.left;
+
+            } else {
+                // go to right
+                if (!currentNode.right) return undefined;
+                if (currentNode.right.val === val) return currentNode.right;
+                currentNode = currentNode.right;
+            }
+        }
+    }
+
+    // Teachers Solution for find
+    find(val) {
+        if (!this.root) return undefined
+        let currentNode = this.root;
+        let found = false;
+
+        while (currentNode && !found) {
+            if (currentNode.val > val) {
+                currentNode = currentNode.left;
+            } else if (currentNode.val < val) {
+                currentNode = currentNode.right;
+            } else {
+                found = true;
+            }
+        }
+
+        if (!currentNode) return undefined;
+        return currentNode;
+    }
 }
 
 let tree = new BinarySearchTree();
-tree.insertOne(41);
-tree.insertOne(20);
-tree.insertOne(65);
-tree.insertOne(29);
-tree.insertOne(50);
-tree.insertOne(91);
-tree.insertOne(72);
-tree.insertOne(99);
-tree.insertOne(32);
-tree.insertOne(11);
-tree.insertOne(12);
+tree.insert(41);
+tree.insert(20);
+tree.insert(65);
+tree.insert(29);
+tree.insert(50);
+tree.insert(91);
+tree.insert(72);
+tree.insert(99);
+tree.insert(32);
+tree.insert(11);
+tree.insert(12);
