cd2b7b69406a073e9a6881bbb7b10d8445c4edf8
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-07-15 20:02:15 +0300

    Dijkstra Algorithm finilizing

--- Dijkstra's-Algorithm/weighted-graph.js
+++ Dijkstra's-Algorithm/weighted-graph.js
@@ -1,20 +1,66 @@
-// Simple priority queue
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
+}
+
 class PriorityQueue {
     constructor() {
         this.values = [];
     }
-    enqueue(val, priority) {
-        this.values.push({ val, priority });
-        this.sort();
-    };
-    dequeue() {
-        return this.values.shift();
-    };
-    sort() {
-        this.values.sort((a, b) => a.priority - b.priority);
-    };
-}
 
+    enqueue(val, priority) {
+        let newNode = new Node(val, priority);
+        this.values.push(newNode);
+        let idx = this.values.length - 1;
+
+        let bubbleUp = (node, idx) => {
+            let priority = node.priority;
+            let parentIdx = Math.floor((idx - 1) / 2);
+            if (parentIdx >= 0 && this.values[parentIdx].priority > priority) {
+                this.values[idx] = this.values[parentIdx];
+                this.values[parentIdx] = node;
+                bubbleUp(node, parentIdx);
+            }
+        };
+
+        bubbleUp(newNode, idx);
+    }
+
+    dequeue() {
+        if (this.values.length === 0) return undefined;
+        if (this.values.length === 1) return this.values.pop();
+        let highPriorityNode = this.values[0];
+
+        let last = this.values.pop();
+        this.values[0] = last;
+        let parentIdx = 0;
+
+        let bubbleDown = (parentIdx) => {
+            let leftChildIdx = (2 * parentIdx) + 1;
+            let rightChildIdx = (2 * parentIdx) + 2;
+            let length = this.values.length;
+            let highPriorityChildIdx = null;
+
+            if (leftChildIdx < length && rightChildIdx < length) {
+                highPriorityChildIdx = this.values[leftChildIdx].priority < this.values[rightChildIdx].priority
+                    ? leftChildIdx
+                    : rightChildIdx;
+            } else if (leftChildIdx < length) {
+                highPriorityChildIdx = leftChildIdx;
+            }
+            if (highPriorityChildIdx && this.values[highPriorityChildIdx].priority < this.values[parentIdx].priority) {
+                [this.values[parentIdx], this.values[highPriorityChildIdx]] = [this.values[highPriorityChildIdx], this.values[parentIdx]];
+                bubbleDown(highPriorityChildIdx);
+            }
+        };
+
+        bubbleDown(parentIdx);
+
+        return highPriorityNode;
+    }
+}
 
 class WeightedGraph {
     constructor() {
